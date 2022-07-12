8380a3d676298d121d6232052a7b65309b647d99
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-12 19:30:39 +0200

    feat: added new components to Aside struture.

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
-import { BiGridAlt } from 'react-icons/bi';
-import { FaEnvelopeOpen, FaLayerGroup, FaNewspaper } from 'react-icons/fa';
+import {
+	BiGridAlt,
+	FiActivity,
+	FiArchive,
+	FiBriefcase,
+	FiDatabase,
+	FiGrid,
+	FiHome,
+	FiPackage,
+	FiUser,
+	FiUsers,
+	HiDocumentReport,
+	HiOutlineDocument,
+	HiUsers,
+	HiViewGrid,
+} from 'react-icons/all';
+import {
+	FaEnvelopeOpen,
+	FaLayerGroup,
+	FaNewspaper,
+	FaUsers,
+	FaUsersCog,
+} from 'react-icons/fa';
 import { AsideContainer as Container } from '../styles/components/aside';
 import { Link } from 'react-router-dom';
 
@@ -9,28 +30,46 @@ const Aside: React.FC = (): JSX.Element => {
 		<Container>
 			<nav>
 				<ul>
-					<Link to={'/recipes-blog'}>
+					<Link to={'/admin/dashboard'}>
 						<li title='Dashboard'>
-							<BiGridAlt />
+							<FiHome />
 							<span>DashBoard</span>
 						</li>
 					</Link>
-					<Link to={'/recipes-blog/posts'}>
-						<li title='Publicações'>
-							<FaLayerGroup />
-							<span>Publicações</span>
+					<Link to={'/admin/products'}>
+						<li title='Products'>
+							<FiPackage />
+							<span>Products</span>
 						</li>
 					</Link>
-					<Link to={'/recipes-blog/subscriptors'}>
-						<li title='Subscritores da newsletter'>
-							<FaNewspaper />
-							<span>Subscritores</span>
+					<Link to={'/admin/categories'}>
+						<li title='Categories'>
+							<FiGrid />
+							<span>Categories</span>
 						</li>
 					</Link>
-					<Link to={'/recipes-blog/messages'}>
-						<li title='Mensagens enviadas'>
-							<FaEnvelopeOpen />
-							<span>Mensagens</span>
+					<Link to={'/admin/profile'}>
+						<li title='Profile'>
+							<FiUser />
+							<span>Profile</span>
+						</li>
+					</Link>
+					<Link to={'/admin/employeers'}>
+						<li title='Employeers'>
+							<FiUsers />
+							<span>Employeers</span>
+						</li>
+					</Link>
+					<Link to={'/admin/company'}>
+						<li title='Company'>
+							<FiBriefcase />
+							<span>Company</span>
+						</li>
+					</Link>
+					<Link to={'/admin/reports'}>
+						<li title='Reports'>
+							<FiActivity />
+							<span>Reports</span>
 						</li>
 					</Link>
 				</ul>
